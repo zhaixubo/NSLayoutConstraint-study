aacf64d6427ef226d9b8e47f1a0d1645b8360e39
zhaixubo <113840032+zhaixubo@users.noreply.github.com>
2023-04-18 15:44:37 +0800

    NSLayoutConstraint学习

--- NSLayoutConstraint学习/ViewController.m
+++ NSLayoutConstraint学习/ViewController.m
@@ -15,7 +15,65 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor whiteColor];
+    // 创建父视图
+    UIView *containerView = [UIView new];
+    containerView.translatesAutoresizingMaskIntoConstraints = YES;
+    containerView.backgroundColor = UIColor.yellowColor;
+    containerView.frame = CGRectMake(100, 100, 200, 200);
+    [self.view addSubview:containerView];
+
+    // 创建子视图
+    UIView *redView = [UIView new];
+    redView.translatesAutoresizingMaskIntoConstraints = NO;
+    redView.backgroundColor = UIColor.redColor;
+    [containerView addSubview:redView];
+
+//    // 添加约束
+//    NSArray *constraints = @[
+//        // 红色视图距离父视图顶部20个点
+//        [redView.topAnchor constraintEqualToAnchor:containerView.topAnchor constant:20],
+//        // 红色视图距离左侧20个点
+//        [redView.leadingAnchor constraintEqualToAnchor:containerView.leadingAnchor constant:20],
+//        // 红色视图距离底部20个点
+//        [redView.bottomAnchor constraintEqualToAnchor:containerView.bottomAnchor constant:-20],
+//        // 红色视图宽度为父视图的一半
+//        [redView.widthAnchor constraintEqualToAnchor:containerView.widthAnchor multiplier:0.5]
+//    ];
+//    [NSLayoutConstraint activateConstraints:constraints];
+
+    // 创建约束条件：view2 距离 view1 的四周都有 20 的间距
+    NSLayoutConstraint *constraint1 = [NSLayoutConstraint constraintWithItem:redView
+                                                                      attribute:NSLayoutAttributeRight
+                                                                      relatedBy:NSLayoutRelationEqual
+                                                                         toItem:containerView
+                                                                      attribute:NSLayoutAttributeRight
+                                                                     multiplier:1.0
+                                                                       constant:-20];
+    NSLayoutConstraint *constraint2 = [NSLayoutConstraint constraintWithItem:redView
+                                                                      attribute:NSLayoutAttributeBottom
+                                                                      relatedBy:NSLayoutRelationEqual
+                                                                         toItem:containerView
+                                                                      attribute:NSLayoutAttributeBottom
+                                                                     multiplier:1.0
+                                                                       constant:-20];
+    NSLayoutConstraint *constraint3 = [NSLayoutConstraint constraintWithItem:redView
+                                                                      attribute:NSLayoutAttributeLeft
+                                                                      relatedBy:NSLayoutRelationEqual
+                                                                         toItem:containerView
+                                                                      attribute:NSLayoutAttributeLeft
+                                                                     multiplier:1.0
+                                                                       constant:20];
+    NSLayoutConstraint *constraint4 = [NSLayoutConstraint constraintWithItem:redView
+                                                                      attribute:NSLayoutAttributeTop
+                                                                      relatedBy:NSLayoutRelationEqual
+                                                                         toItem:containerView
+                                                                      attribute:NSLayoutAttributeTop
+                                                                     multiplier:1.0
+                                                                       constant:20];
+
+    // 将约束条件添加到视图上
+    [self.view addConstraints:@[constraint1, constraint2, constraint3, constraint4]];
 }
 
 
